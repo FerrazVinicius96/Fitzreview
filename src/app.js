9621035009868cf0f4eda1568adaa247112d7dea
FerrazVinicius96 <vesf.96@gmail.com>
2026-07-14 14:42:54 -0300

    Minor changes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
-const usuarioRouter = require('./routes/UsuarioRoutes.js');
+const UsuarioRoutes = require('./routes/UsuarioRoutes.js');
 const pool = require('./db/db.js');
 
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use('', usuarioRouter);
+app.use('', UsuarioRoutes);
 
 pool.query('SELECT NOW()', (err, res) => {
 	if (err) {
